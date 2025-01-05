6a7892c39d8c4f2715d57a7698f3b7edd5d99115
Your Name <you@example.com>
2025-01-05 04:28:44 +0100

    dbconfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,28 @@
 const mysql2 = require("mysql2");
 
-const dbConection = mysql2.createPool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  host: "localhost",
-  password: process.env.PASSWORD,
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+console.log("DB_HOST:", process.env.DB_HOST || "localhost");
+
+const dbConnection = mysql2.createPool({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || "localhost",
+  password: process.env.DB_PASSWORD,
   connectionLimit: 10,
 });
 
+module.exports = dbConnection.promise();
 
-module.exports = dbConection.promise();
+// const mysql2 = require("mysql2");
+
+// const dbConection = mysql2.createPool({
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   host: "localhost",
+//   password: process.env.PASSWORD,
+//   connectionLimit: 10,
+// });
+
+// module.exports = dbConection.promise();
