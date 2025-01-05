398121c94cdc86414bd5ccc01cf3120b0c316bdf
Your Name <you@example.com>
2025-01-05 22:58:35 +0100

    DB

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,19 +1,32 @@
-const mysql2 = require("mysql2");
-
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_DATABASE:", process.env.DB_DATABASE);
-console.log("DB_HOST:", process.env.DB_HOST || "localhost");
-
 const dbConnection = mysql2.createPool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST || "localhost",
   password: process.env.DB_PASSWORD,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: true, // Set to false if using self-signed certificates
+  },
 });
 
-module.exports = dbConnection.promise();
+
+
+//const mysql2 = require("mysql2");
+
+// console.log("DB_USER:", process.env.DB_USER);
+// console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+// console.log("DB_DATABASE:", process.env.DB_DATABASE);
+// console.log("DB_HOST:", process.env.DB_HOST || "localhost");
+
+// const dbConnection = mysql2.createPool({
+//   user: process.env.DB_USER,
+//   database: process.env.DB_DATABASE,
+//   host: process.env.DB_HOST || "localhost",
+//   password: process.env.DB_PASSWORD,
+//   connectionLimit: 10,
+// });
+
+// module.exports = dbConnection.promise();
 
 // const mysql2 = require("mysql2");
 
